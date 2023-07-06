789aa95f899276fd2ba6ba4f768c141e0e5db7c0
Damjan Hudobreznik <damjan.hudobreznik@gmail.com>
2023-07-06 10:20:21 +0200

    Add player movement constraints.

--- player.py
+++ player.py
@@ -2,11 +2,12 @@ import pygame
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos):
+    def __init__(self, pos, screen_width, speed):
         super().__init__()
         self.image = pygame.image.load('graphics/player.png').convert_alpha()
         self.rect = self.image.get_rect(midbottom=pos)
-        self.speed = 5
+        self.speed = speed
+        self.screen_width = screen_width
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -16,5 +17,12 @@ class Player(pygame.sprite.Sprite):
         elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
             self.rect.x -= self.speed
 
+    def constraint(self):
+        if self.rect.left <= 0:
+            self.rect.left = 0
+        elif self.rect.right >= self.screen_width:
+            self.rect.right = self.screen_width
+
     def update(self):
         self.get_input()
+        self.constraint()
