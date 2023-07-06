3dba7507d9a7805868b474ae52b2308e04f798e6
Damjan Hudobreznik <damjan.hudobreznik@gmail.com>
2023-07-06 14:52:56 +0200

    Draw basic obstacle from a shape.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from player import Player
+import obstacle
 
 
 class Game:
@@ -8,11 +9,29 @@ class Game:
         player_sprite = Player((screen_width / 2, screen_height), screen_width, screen_height, 5)
         self.player = pygame.sprite.GroupSingle(player_sprite)
 
+        self.shape = obstacle.shape
+        self.block_size = 6
+        self.blocks = pygame.sprite.Group()
+        self.create_obstacle()
+
+    def create_obstacle(self):
+        for row_index, row in enumerate(self.shape):
+            for col_index, col in enumerate(row):
+                if col == 'x':
+                    block = obstacle.Block(
+                        self.block_size,
+                        (241, 79, 80),
+                        col_index * self.block_size,
+                        row_index * self.block_size
+                    )
+                    self.blocks.add(block)
+
     def run(self):
         self.player.update()
 
         self.player.sprite.lasers.draw(screen)
         self.player.draw(screen)
+        self.blocks.draw(screen)
 
 
 if __name__ == '__main__':
