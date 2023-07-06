24276d4417ea06acd319eeea8501a58f39a1c860
damjanh <damjan.hudobreznik@gmail.com>
2023-07-06 22:41:30 +0200

    Implement alien enemies.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from player import Player
 import obstacle
+from alien import Alien
 
 
 class Game:
@@ -14,7 +15,10 @@ class Game:
         self.blocks = pygame.sprite.Group()
         self.obstacle_amount = 4
         self.obstacle_x_positions = [num * (screen_width / self.obstacle_amount) for num in range(self.obstacle_amount)]
-        self.create_multiple_obstacle(*self.obstacle_x_positions, start_x=screen_width / 15 , start_y=480)
+        self.create_multiple_obstacle(*self.obstacle_x_positions, start_x=screen_width / 15, start_y=480)
+
+        self.aliens = pygame.sprite.Group()
+        self.create_aliens(rows=6, cols=8)
 
     def create_multiple_obstacle(self, *offset, start_x, start_y, ):
         for offset_x in offset:
@@ -32,12 +36,26 @@ class Game:
                     )
                     self.blocks.add(block)
 
+    def create_aliens(self, rows, cols, x_distance=60, y_distance=48, x_offset=70, y_offset=100):
+        for row_index, row in enumerate(range(rows)):
+            for col_index, col in enumerate(range(cols)):
+                x = col_index * x_distance + x_offset
+                y = row_index * y_distance + y_offset
+                if row_index == 0:
+                    alien_sprite = Alien('yellow', x, y)
+                elif 1 <= row_index <= 2:
+                    alien_sprite = Alien('green', x, y)
+                else:
+                    alien_sprite = Alien('red', x, y)
+                self.aliens.add(alien_sprite)
+
     def run(self):
         self.player.update()
 
         self.player.sprite.lasers.draw(screen)
         self.player.draw(screen)
         self.blocks.draw(screen)
+        self.aliens.draw(screen)
 
 
 if __name__ == '__main__':
